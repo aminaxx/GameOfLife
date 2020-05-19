308c27be09a371a9980b068ef763af0b97790e63
gulshan29 <61164121+gulshan29@users.noreply.github.com>
2020-05-20 00:25:04 +0400

    circular mode

---
+++ DefaultPosition/new_ut/mylib/header.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+       #include <unistd.h>
+//#include "cut.h"
+#define MAX 5
+int getTop(int i,int j,int arr[MAX][MAX]);
+int getBottom(int i,int j,int arr[MAX][MAX]);
+int getRight(int i,int j,int arr[MAX][MAX]);
+int getLeft(int i,int j,int arr[MAX][MAX]);
+int getLeftTop(int i,int j,int arr[MAX][MAX]);
+int getRightTop(int i,int j,int arr[MAX][MAX]);
+int getLeftBottom(int i,int j,int arr[MAX][MAX]);
+int getRightBottom(int i,int j,int arr[MAX][MAX]);
+bool decide_mode(int i, int j, int array[MAX][MAX]);
+#endif
